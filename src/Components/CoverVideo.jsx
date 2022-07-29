d24c41bd9d123355c24acb44fd24bd16730d93c6
wwdfdo <wwdfdo@gmail.com>
2022-07-30 02:01:44 +0530

    third commit fourth

--- src/Components/CoverVideo.jsx
+++ src/Components/CoverVideo.jsx
@@ -21,7 +21,7 @@ const CoverVideo = () => {
           <h2 className="w-[100px] md:w-[150px] text-center">REAL ECONOMICS</h2>
         </div>
 
-        <div className="md:w-3/5 w-[95%] border-2 bg-white mx-auto rounded-3xl   text-black flex md:flex-row flex-col md:justify-around gap-6 md:gap-0 py-20 md:py-0  items-center absolute bottom-0 right-0 left-0 z-[5]">
+        <div className="md:w-3/5 w-[95%] border-2 bg-white mx-auto rounded-3xl h-[20vh]   text-black flex md:flex-row flex-col md:justify-around gap-6 md:gap-0 py-20 md:py-0  items-center absolute bottom-0 right-0 left-0 z-[5]">
           <div className="md:w-1/3">
             <p>REQUEST EARLY ACCESS</p>
             <p>Claim your invitation to the Mega World metaverse demo today.</p>
